2e723991684fdcc642fec8cb3ab24b1371d34d57
bashartech <144168218+bashartech@users.noreply.github.com>
2024-12-30 01:45:58 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images: {
         domains: ["fakestoreapi.com","cdn.dummyjson.com"]
-    }
+    },
+    env:{
+        API_URL : process.env.API_URL
+    },
 };
 
 export default nextConfig;
